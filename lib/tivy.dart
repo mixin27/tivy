b13a6d72140ca521e7cc227e0a5a403a21a6dd54
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-10-17 10:26:18 +0630

    style: code formatting

--- lib/tivy.dart
+++ lib/tivy.dart
@@ -1,4 +1,4 @@
-/// Utility package for getting video quality urls for vimeo and youtube. 
+/// Utility package for getting video quality urls for vimeo and youtube.
 library tivy;
 
 export 'src/models/models.dart';
