21a6eadfc57fe07a34ed1996624c14fca3b43d65
mixin27 <kyawzayartun.dev@gmail.com>
2024-12-12 02:04:53 +0630

    remove exporting some files

--- lib/tivy.dart
+++ lib/tivy.dart
@@ -1,8 +1,6 @@
 /// Utility package for getting video quality urls for vimeo and youtube.
 library tivy;
 
-export 'src/apis/vimeo_api_client.dart';
 export 'src/models/models.dart';
-export 'src/repositories/vimeo/vimeo_repository.dart';
 export 'src/tivy.dart';
 export 'src/utls/result.dart';
